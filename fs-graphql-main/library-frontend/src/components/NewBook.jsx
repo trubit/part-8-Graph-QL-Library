50c4f79b8ae09c85c92a8e949b19b81e5ef389b3
Ezika trust chidi <trustezika831@gmail.com>
2026-06-01 18:49:22 +0100

    Adding a book
    Co-Authored-By: Ezika Trust Chidi <trustezika831@gmail.com>

--- fs-graphql-main/library-frontend/src/components/NewBook.jsx
+++ fs-graphql-main/library-frontend/src/components/NewBook.jsx
@@ -1,52 +1,71 @@
-import { useState } from 'react'
+import { useState } from "react";
+import { useMutation } from "@apollo/client/react";
+import { CREATE_BOOK, ALL_AUTHORS, ALL_BOOKS } from "../queries";
 
-const NewBook = (props) => {
-  const [title, setTitle] = useState('')
-  const [author, setAuthor] = useState('')
-  const [published, setPublished] = useState('')
-  const [genre, setGenre] = useState('')
-  const [genres, setGenres] = useState([])
+const NewBook = ({ show }) => {
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const [published, setPublished] = useState("");
+  const [genre, setGenre] = useState("");
+  const [genres, setGenres] = useState([]);
 
-  if (!props.show) {
-    return null
+  const [createBook] = useMutation(CREATE_BOOK, {
+    refetchQueries: [{ query: ALL_BOOKS }, { query: ALL_AUTHORS }],
+    onError: (error) => {
+      console.error("Error creating book:", error);
+    },
+  });
+
+  if (!show) {
+    return null;
   }
 
   const submit = async (event) => {
-    event.preventDefault()
+    event.preventDefault();
 
-    console.log('add book...')
+    await createBook({
+      variables: {
+        title,
+        author,
+        published: parseInt(published),
+        genres,
+      },
+    });
 
-    setTitle('')
-    setPublished('')
-    setAuthor('')
-    setGenres([])
-    setGenre('')
-  }
+    /*console.log("add book...");*/
+
+    setTitle("");
+    setPublished("");
+    setAuthor("");
+    setGenres([]);
+    setGenre("");
+  };
 
   const addGenre = () => {
-    setGenres(genres.concat(genre))
-    setGenre('')
-  }
+    setGenres(genres.concat(genre));
+    setGenre("");
+  };
 
   return (
     <div>
+      <h2>Add new book</h2>
       <form onSubmit={submit}>
         <div>
-          title
+          <label>title</label>
           <input
             value={title}
             onChange={({ target }) => setTitle(target.value)}
           />
         </div>
         <div>
-          author
+          <label>author</label>
           <input
             value={author}
             onChange={({ target }) => setAuthor(target.value)}
           />
         </div>
         <div>
-          published
+          <label>published</label>
           <input
             type="number"
             value={published}
@@ -54,6 +73,7 @@ const NewBook = (props) => {
           />
         </div>
         <div>
+          <label>genre</label>
           <input
             value={genre}
             onChange={({ target }) => setGenre(target.value)}
@@ -62,11 +82,11 @@ const NewBook = (props) => {
             add genre
           </button>
         </div>
-        <div>genres: {genres.join(' ')}</div>
+        <div>genres: {genres.join(" ")}</div>
         <button type="submit">create book</button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default NewBook
+export default NewBook;
